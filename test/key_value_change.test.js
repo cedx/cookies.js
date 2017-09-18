e1eaccafd8d9bf2524fa451c94d7701996c79f41
Cédric Belin <cedric@belin.io>
2017-09-18 12:34:14 +0200

    Added the unit tests of the `KeyValueChange` class

---
+++ test/key_value_change.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const {expect} = require('chai');
+const {KeyValueChange} = require('../src');
+
+/**
+ * @test {KeyValueChange}
+ */
+describe('KeyValueChange', () => {
+
+  /**
+   * @test {KeyValueChange#toJSON}
+   */
+  describe('#toJSON()', () => {
+    it('should return a map with default values for a newly created instance', () => {
+      expect(new KeyValueChange('').toJSON()).to.be.an('object').that.deep.equal({
+        currentValue: null,
+        key: '',
+        previousValue: null
+      });
+    });
+
+    it('should return a non-empty map for an initialized instance', () => {
+      expect(new KeyValueChange('foo', {currentValue: 'bar', previousValue: 'baz'}).toJSON()).to.be.an('object').that.deep.equal({
+        currentValue: 'bar',
+        key: 'foo',
+        previousValue: 'baz'
+      });
+    });
+  });
+
+  /**
+   * @test {KeyValueChange#toString}
+   */
+  describe('#toString()', () => {
+    let data = new KeyValueChange('foo', {currentValue: 'bar', previousValue: 'baz'}).toString();
+
+    it('should start with the class name', () => {
+      expect(data.startsWith('KeyValueChange {')).to.be.true;
+    });
+
+    it('should contain the instance properties', () => {
+      expect(data).to.contain('"currentValue":"bar"')
+        .and.contain('"key":"foo"')
+        .and.contain('"previousValue":"baz"');
+    });
+  });
+});
