2abf0dbc7752022c449859661c0c902975a2523e
Cédric Belin <cedric@belin.io>
2018-08-16 00:24:59 +0200

    Updated the project settings

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,15 @@
-const {join} from 'path');
-const commonjs from 'rollup-plugin-commonjs');
-const nodeResolve from 'rollup-plugin-node-resolve');
+const {join} = require('path');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
 
 module.exports = config => config.set({
   browsers: ['Chrome'],
+  coverageReporter: {
+    dir: 'var',
+    includeAllSources: true,
+    subdir: '.',
+    type: 'lcovonly'
+  },
   files: ['src/**/*.ts', 'test/**/*.ts'],
   frameworks: ['mocha', 'karma-typescript'],
   karmaTypescriptConfig: {
@@ -14,25 +20,11 @@ module.exports = config => config.set({
     'src/**/*.ts': ['karma-typescript', 'coverage'],
     'test/**/*.ts': ['karma-typescript']
   },
-  reporters: ['progress', 'karma-typescript']
+  reporters: ['progress', 'karma-typescript', 'coverage'],
+  singleRun: true
 });
 
 module.exports = config => config.set({
-  browsers: ['Chrome'],
-  client: {mocha: {opts: true}},
-  frameworks: ['mocha'],
-  files: ['test/**/*.ts'],
-  reporters: ['progress', 'coverage'],
-  coverageReporter: {
-    dir: 'var',
-    includeAllSources: true,
-    subdir: '.',
-    type: 'lcovonly'
-  },
-  preprocessors: {
-    'lib/**/*.js': ['coverage'],
-    'test/**/*.ts': ['typescript']
-  },
   rollupPreprocessor: {
     onwarn: warning => {
       if (warning.code == 'CIRCULAR_DEPENDENCY' && warning.importer.includes(join('node_modules', 'chai'))) return;
