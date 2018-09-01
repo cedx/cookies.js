c4cb2aecae031b302dd1684290b8482cd740bc9b
Cédric Belin <cedric@belin.io>
2018-09-01 03:10:23 +0200

    Ported the build system to TypeScript

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,4 +1,4 @@
-module.exports = config => config.set({
+export default (config: any) => config.set({
   browsers: ['Chrome'],
   files: ['src/**/*.ts', 'test/**/*.ts'],
   frameworks: ['mocha', 'karma-typescript'],
