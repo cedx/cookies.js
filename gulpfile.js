920971c562e27d01d75a25ec86a0056e8d220612
Cédric Belin <cedric@belin.io>
2019-02-26 17:57:47 +0100

    Using destructuring to improve the Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 'use strict';
-
 const {spawn} = require('child_process');
 const del = require('del');
 const {promises} = require('fs');
-const gulp = require('gulp');
+const {dest, series, src, task, watch} = require('gulp');
 const replace = require('gulp-replace');
 const {delimiter, normalize, resolve} = require('path');
 
@@ -21,7 +20,7 @@ const sources = ['*.js', 'example/*.ts', 'src/**/*.ts', 'test/**/*.ts'];
 /**
  * Builds the project.
  */
-gulp.task('build', async () => {
+task('build', async () => {
   await _exec('tsc', ['--project', 'src/tsconfig.json']);
   await _exec('rollup', ['--config']);
   return _exec('minify', ['build/cookies.js', '--out-file=build/cookies.min.js']);
@@ -30,19 +29,19 @@ gulp.task('build', async () => {
 /**
  * Deletes all generated files and reset any saved state.
  */
-gulp.task('clean', () => del(['.nyc_output', 'build', 'coverage', 'doc/api', 'lib', 'var/**/*', 'web']));
+task('clean', () => del(['.nyc_output', 'build', 'coverage', 'doc/api', 'lib', 'var/**/*', 'web']));
 
 /**
  * Uploads the results of the code coverage.
  */
-gulp.task('coverage:fix', () => gulp.src('var/lcov.info').pipe(replace(/cookies\.ts([/\\])src/g, 'cookies.js$1src')).pipe(gulp.dest('var')));
-gulp.task('coverage:upload', () => _exec('coveralls', ['var/lcov.info']));
-gulp.task('coverage', gulp.series('coverage:fix', 'coverage:upload'));
+task('coverage:fix', () => src('var/lcov.info').pipe(replace(/cookies\.ts([/\\])src/g, 'cookies.js$1src')).pipe(dest('var')));
+task('coverage:upload', () => _exec('coveralls', ['var/lcov.info']));
+task('coverage', series('coverage:fix', 'coverage:upload'));
 
 /**
  * Builds the documentation.
  */
-gulp.task('doc', async () => {
+task('doc', async () => {
   await promises.copyFile('CHANGELOG.md', 'doc/about/changelog.md');
   await promises.copyFile('LICENSE.md', 'doc/about/license.md');
   await _exec('typedoc');
@@ -52,27 +51,27 @@ gulp.task('doc', async () => {
 /**
  * Fixes the coding standards issues.
  */
-gulp.task('fix', () => _exec('tslint', ['--fix', ...sources]));
+task('fix', () => _exec('tslint', ['--fix', ...sources]));
 
 /**
  * Performs the static analysis of source code.
  */
-gulp.task('lint', () => _exec('tslint', sources));
+task('lint', () => _exec('tslint', sources));
 
 /**
  * Starts the development server.
  */
-gulp.task('serve', () => _exec('http-server', ['example', '-o']));
+task('serve', () => _exec('http-server', ['example', '-o']));
 
 /**
  * Runs the test suites.
  */
-gulp.task('test', () => _exec('karma', ['start']));
+task('test', () => _exec('karma', ['start']));
 
 /**
  * Upgrades the project to the latest revision.
  */
-gulp.task('upgrade', async () => {
+task('upgrade', async () => {
   await _exec('git', ['reset', '--hard']);
   await _exec('git', ['fetch', '--all', '--prune']);
   await _exec('git', ['pull', '--rebase']);
@@ -83,15 +82,15 @@ gulp.task('upgrade', async () => {
 /**
  * Watches for file changes.
  */
-gulp.task('watch', () => {
-  gulp.watch('src/**/*.ts', {ignoreInitial: false}, gulp.task('build'));
-  gulp.watch('test/**/*.ts', gulp.task('test'));
+task('watch', () => {
+  watch('src/**/*.ts', {ignoreInitial: false}, task('build'));
+  watch('test/**/*.ts', task('test'));
 });
 
 /**
  * Runs the default tasks.
  */
-gulp.task('default', gulp.task('build'));
+task('default', task('build'));
 
 /**
  * Spawns a new process using the specified command.
