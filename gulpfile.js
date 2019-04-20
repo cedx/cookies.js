1e1467a50e3d71de2e14e2932093c1959e78b015
Cédric Belin <cedric@belin.io>
2019-04-20 17:27:20 +0200

    Optimized the Karma settings

--- gulpfile.js
+++ gulpfile.js
@@ -25,12 +25,10 @@ task('build', async () => {
 });
 
 /** Deletes all generated files and reset any saved state. */
-task('clean', () => del(['build', 'coverage', 'doc/api', 'lib', 'var/**/*', 'web']));
+task('clean', () => del(['build', 'doc/api', 'lib', 'var/**/*', 'web']));
 
 /** Uploads the results of the code coverage. */
-task('coverage:fix', () => src('var/lcov.info').pipe(replace(/cookies\.ts([/\\])src/g, 'cookies.js$1src')).pipe(dest('var')));
-task('coverage:upload', () => _exec('coveralls', ['var/lcov.info']));
-task('coverage', series('coverage:fix', 'coverage:upload'));
+task('coverage', () => _exec('coveralls', ['var/lcov.info']));
 
 /** Builds the documentation. */
 task('doc', async () => {
