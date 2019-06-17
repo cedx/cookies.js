05975724438860d22f9dc9799d9abf8fb7b7f422
Cédric Belin <cedric@belin.io>
2019-06-17 21:32:31 +0200

    Updated the build system

--- etc/rollup.js
+++ etc/rollup.js
@@ -1,11 +1,10 @@
-import {join} from 'path';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import {resolve} from 'path';
 
 export default {
-  input: join(__dirname, '../lib/index.js'),
-  output: {file: join(__dirname, '../build/cookies.js'), format: 'iife', name: 'cookies'},
-  plugins: [resolve(), commonjs({
-    namedExports: {'node_modules/eventemitter3/index.js': ['EventEmitter']}
-  })]
+  input: resolve(__dirname, '../lib/index.js'),
+  output: {
+    file: resolve(__dirname, '../build/cookies.js'),
+    format: 'iife',
+    name: 'cookies'
+  }
 };
