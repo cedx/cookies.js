dd2aa91491601ff1009415e17a332730ff9abd44
Cédric Belin <cedric@belin.io>
2022-06-09 14:52:02 +0200

    Added a sample for iteration

---
+++ example/iteration.js
@@ -0,0 +1,31 @@
+/* eslint-disable curly, lines-around-comment */
+import {CookieStore} from "@cedx/cookies";
+
+// Loop over all entries of the cookie store.
+const cookieStore = new CookieStore()
+	.set("foo", "bar")
+	.set("bar", "baz")
+	.set("baz", "qux");
+
+for (const [key, value] of cookieStore) {
+	console.log(`${key} => ${value}`);
+	// Round 1: "foo => bar"
+	// Round 2: "bar => baz"
+	// Round 3: "baz => qux"
+}
+
+cookieStore.clear();
+
+// Loop over entries of the cookie store that use the same key prefix.
+cookieStore
+	.set("foo", "bar")
+	.set("prefix:bar", "baz");
+
+const prefixedCookieStore = new CookieStore({keyPrefix: "prefix:"})
+	.set("baz", "qux");
+
+for (const [key, value] of prefixedCookieStore) {
+	console.log(`${key} => ${value}`);
+	// Round 1: "bar => baz"
+	// Round 2: "baz => qux"
+}
