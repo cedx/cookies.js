03fb16b267aff899954f603b4c71acd44cc8b9d7
Cédric Belin <cedric@belin.io>
2023-01-28 00:45:09 +0100

    Code formatting

--- src/same_site.js
+++ src/same_site.js
@@ -4,12 +4,18 @@
  */
 export const SameSite = Object.freeze({
 
-	/** Only send cookies for top level navigation requests. */
+	/**
+	 * Only send cookies for top level navigation requests.
+	 */
 	lax: "lax",
 
-	/** No restrictions on cross-site requests. */
+	/**
+	 * No restrictions on cross-site requests.
+	 */
 	none: "none",
 
-	/** Prevents the cookie from being sent to the target site in all cross-site browsing context. */
+	/**
+	 * Prevents the cookie from being sent to the target site in all cross-site browsing context.
+	 */
 	strict: "strict"
 });
