b7d7111a4bd7a1587706e247dded11d7a49a874b
Cédric Belin <cedric@belin.io>
2023-02-20 11:04:53 +0100

    Updated the typings

--- src/cookie_store.js
+++ src/cookie_store.js
@@ -86,9 +86,8 @@ export class CookieStore extends EventTarget {
 
 	/**
 	 * Gets the deserialized value associated with the specified key.
-	 * @template T
 	 * @param {string} key The cookie name.
-	 * @returns {T|null} The cookie value, or `null` if the key does not exist or the value cannot be deserialized.
+	 * @returns {any} The cookie value, or `null` if the key does not exist or the value cannot be deserialized.
 	 */
 	getObject(key) {
 		try { return JSON.parse(this.get(key) ?? ""); }
@@ -126,15 +125,14 @@ export class CookieStore extends EventTarget {
 
 	/**
 	 * Looks up the deserialized value of the specified key, or add a new serialized value if it isn't there.
-	 * @template T
 	 * @param {string} key The cookie name.
-	 * @param {() => T} ifAbsent A function producing the new cookie value.
+	 * @param {() => unknown} ifAbsent A function producing the new cookie value.
 	 * @param {import("./cookie_options.js").CookieOptionsParams} options The cookie options.
-	 * @returns {T} The deserialized value associated with the key.
+	 * @returns {any} The deserialized value associated with the key.
 	 */
 	putObjectIfAbsent(key, ifAbsent, options = {}) {
 		if (!this.has(key)) this.setObject(key, ifAbsent(), options);
-		return /** @type {T} */ (this.getObject(key));
+		return this.getObject(key);
 	}
 
 	/**
