140dc515b72ebe31824b62bc1649e3a15f1e971e
Cédric Belin <cedx@outlook.com>
2025-09-16 23:09:29 +0200

    Update the code sample

--- example/Iteration.js
+++ example/Iteration.js
@@ -2,24 +2,23 @@ import {CookieStore} from "@cedx/cookies";
 import console from "node:console";
 
 // Loop over all entries of the cookie store.
-const cookieStore = new CookieStore()
-	.set("foo", "bar")
-	.set("bar", "baz")
-	.set("baz", "qux");
+const cookieStore = new CookieStore;
+await cookieStore.set("foo", "bar");
+await cookieStore.set("bar", "baz");
+await cookieStore.set("baz", "qux");
 
-for (const [key, value] of cookieStore) console.log(`${key} => ${value}`);
+for await (const [key, value] of cookieStore) console.log(`${key} => ${value}`);
 // Round 1: "foo => bar"
 // Round 2: "bar => baz"
 // Round 3: "baz => qux"
 
-cookieStore.clear();
+await cookieStore.clear();
 
 // Loop over entries of the cookie store that use the same key prefix.
-cookieStore
-	.set("foo", "bar")
-	.set("prefix:bar", "baz");
+await cookieStore.set("foo", "bar");
+await cookieStore.set("prefix:bar", "baz");
 
-const prefixedStore = new CookieStore({keyPrefix: "prefix:"}).set("baz", "qux");
-for (const [key, value] of prefixedStore) console.log(`${key} => ${value}`);
+const prefixedStore = await new CookieStore({keyPrefix: "prefix:"}).set("baz", "qux");
+for await (const [key, value] of prefixedStore) console.log(`${key} => ${value}`);
 // Round 1: "bar => baz"
 // Round 2: "baz => qux"
